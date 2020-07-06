cced6c2d2d84b97505e8431a7f1eb95649e41df9
Maya Luna Celeste <valkyriealts@gmail.com>
2020-07-06 00:36:21 -0400

    Initial Commit

---
+++ NeuralNet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NeuralNet
+//
+//  Created by Maya Luna Celeste on 7/6/20.
+//  Copyright © 2020 Maya Luna Celeste. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
